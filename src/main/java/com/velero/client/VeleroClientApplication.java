0a762c19b540b392b355b999356723cac0e1a1fe
misskaori <liuweizhe@hotmail.com>
2022-12-20 18:08:37 +0800

    Initial commit

---
+++ src/main/java/com/velero/client/VeleroClientApplication.java
@@ -0,0 +1,13 @@
+package com.velero.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeleroClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeleroClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/velero/client/VeleroClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velero.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeleroClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
